e2151a6616b2cd1b112b9bd5339b02f3b4526917
LiamTriaca <Liam.Triaca79@gmail.com>
2019-07-16 21:13:43 -0300

    Maquetando el reproductor

--- Nuevito - copia/nerdflix/js/peliculas.class.js
+++ Nuevito - copia/nerdflix/js/peliculas.class.js
@@ -18,11 +18,27 @@ class Pelicula {
     	elemento.querySelector("h4").innerText= this.titulo
     	elemento.querySelector("p").innerText=this.estreno
     	elemento.querySelector("img").src=this.poster
+        //3) Generar comportamiento reproductor mediante un closure
+        elemento.querySelector("a").onclick= ()=>{
+            console.log(this)
+            //A PARTIR DE ESTE PUNTO NUESTRO THIS ES LA PE LI CU LA QUE ELIJAMOS 
+    
 
-    	//3)Ocultar elemento
+        let reproductor= document.querySelector("#playMovie")
+
+        console.log(reproductor)
+        reproductor.querySelector("#titulo").innerText=`${this.titulo} ${this.estreno}`
+        reproductor.querySelector("#descripcion").innerText=this.descripcion
+        reproductor.querySelector("#imagen").src=this.poster
+        reproductor.querySelector("iframe").src=this.trailer
+
+        }
+
+
+    	//4)Ocultar elemento
     	elemento.classList.remove("hide")
 
-    	//4) Anexar elementos
+    	//5) Anexar elementos
 
     	document.querySelector("#peliculas").appendChild(elemento)
 
